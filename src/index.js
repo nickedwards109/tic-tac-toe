d2895aaf2e748c6001c9d93bce5132ebf2b8efab
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-02 16:37:25 -0600

    Set up controlled component architecture
    This commit implements state of clicked-ness that is held in the parent (Board) and passed down to the children (Squares). The child Squares can notify the parent Board of a state change via click event handlers.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,34 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square"></button>
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -27,14 +47,14 @@ class Board extends React.Component {
           {this.renderSquare(2)}
         </div>
         <div>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
